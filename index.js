00ed8d99e4a74b6494b51fe0e6cdc26cc7e6ad78
anders-g-hansen <anders.g.hansen@medtronic.com>
2017-02-16 14:12:16 -0600

    Updated so that when the children has a length of 1 the 0th element is added.

--- index.js
+++ index.js
@@ -268,6 +268,8 @@ export default class Carousel extends Component {
       // so we add first and second page again to the end
       pages.push(children[0]);
       pages.push(children[1]);
+    } else if (children.length === 1) {
+      pages.push(children[0]);
     } else if (children) {
       pages.push(children);
     } else {
