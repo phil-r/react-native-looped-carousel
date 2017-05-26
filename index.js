d2af377111584bc6fc99c91647825ac97e3c31f5
Viktor Seč <hey@viktorsec.com>
2017-05-26 17:04:46 +0200

    Fix children rerendering (#96)
    * Fix children rerendering
    
    * Change dependency

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import {
   View,
   TouchableWithoutFeedback,
 } from 'react-native';
+import { isEqual } from 'lodash.isequal';
 
 
 const PAGE_CHANGE_DELAY = 4000;
@@ -103,8 +104,9 @@ export default class Carousel extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    if (this.props.children !== nextProps.children) {
+    if (!isEqual(this.props.children, nextProps.children)) {
       let childrenLength = 0;
+      this.setState({ currentPage: 1 });
       if (nextProps.children) {
         const length = React.Children.count(nextProps.children);
         childrenLength = length || 1;
